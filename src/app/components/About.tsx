fb3eeeefc0bebbdf958de9f0572bf5f45f0b379c
George Rhodes <gear522dev@gmail.com>
2024-08-01 18:20:30 +0100

    slight fixes

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const aboutText =
   "My name is George Alexander Rhodes, but I go by Alex with family and friends. I was born in Jersey but have lived the majority of my life in the Isle of Man. The IoM is famous for the TT motorbike race, which I highly recommend watching some PoV videos of! The average speed is around 135 mph! But I will always reccomend Jersey first. We have actual beaches and whilst winter is wet, IoM is like a permanent Jersey Winter. \n\n Whilst I was here in Jersey till the age of ten, I went to St George's Preparatory School. Then after moving to the Isle of Man, I went to the secondary school King Williams College and did my GCSEs and the International Baccalaureate instead of A Levels. I took English, Maths, Physics, Geography, German, and Economics with a score of 34 points. \n\nI have always had an interest in Far East Asian culture and have been lucky enough to go to Japan several times. The first time I was working in a guest house there in exchange for free bedding, second time was a home stay, and then my year abroad during my Business and Japanese course at Newcastle University, which was unfortunately impacted because of COVID. \n\nI have always had an interest in programming, logical thinking, and how to maximise efficiency. This is likely due to my passion for gaming and technology. After finishing my degree, I started self studying coding whilst working for the COVID Department of the Jersey Government, which landed me my first position in under five months. I want to further refine my skills in web app development, but also look more into data analysis, data science, and how large data sets can inform business decisions based on trends, analysis, and forming predictive models. For this reason I am currently revisiting Python and learning to use the packages such as matplotlib, pandas, numpy, etc, as well as Microsoft Power BI to create interactive dashboards and reports with the resulting data.";
@@ -15,6 +16,43 @@ export default function About() {
         <p id="about-content-text" className="text-lg whitespace-pre-wrap">
           {aboutText}
         </p>
+      </div>
+      <div
+        id="language-icons"
+        className="flex flex-col sm:flex-row sm:justify-between"
+      >
+        <Image
+          className=" rounded-xl m-3"
+          id="language-pic"
+          width={100}
+          height={45}
+          src="/rails-logo.png_large"
+          alt="rails-logo"
+        />
+        <Image
+          className=" rounded-xl m-3"
+          id="language-pic"
+          width={100}
+          height={45}
+          src="/typescript-logo.png"
+          alt="typescript-logo"
+        />
+        <Image
+          className=" rounded-xl m-3 bg-transparent"
+          id="language-pic"
+          width={100}
+          height={45}
+          src="/react-logo.svg"
+          alt="react-logo"
+        />
+        <Image
+          className=" rounded-xl m-3"
+          id="language-pic"
+          width={100}
+          height={45}
+          src="/python-logo.png"
+          alt="python-logo"
+        />
       </div>
     </div>
   );
--- src/app/components/WelcomeBlock.tsx
+++ src/app/components/WelcomeBlock.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const welcomeText =
   'This is my personal website. It will likely always be a work in progress as I plan to add to it as I learn more and develop myself. This is where I will keep some posts about things I am doing or want to do, as well as list my employment history and projects I work on in my spare time. \n\nI have experience in full stack web app development using Typescript and React for frontend development and Ruby on Rails for backend development. I have built a simple project using Django as the backend and plan to do more with it soon. I also have an interest in data analysis, which is why I am learning to use Microsoft Power BI with the intention to take the PL-300 exam. ';
@@ -15,6 +16,20 @@ export default function WelcomeBlock() {
       <div id="welcome-block-content" className="py-6">
         <p className="whitespace-pre-wrap text-lg">{welcomeText}</p>
       </div>
+      <a
+        className="flex w-fit"
+        target="_blank"
+        href="https://learn.microsoft.com/en-us/credentials/certifications/data-analyst-associate/?practice-assessment-type=certification"
+      >
+        <Image
+          className=" rounded-lg m-3"
+          id="power-bi-pic"
+          width={100}
+          height={45}
+          src="/power-bi-logo.png"
+          alt="power-bi-icon"
+        />
+      </a>
     </div>
   );
 }
